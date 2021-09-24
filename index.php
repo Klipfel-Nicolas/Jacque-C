d4ee05ef2c802cbf32a55b740656459f37c38a69
Klipfel-Nicolas <klipfelnicolas86@gmail.com>
2021-09-24 20:41:49 +0200

    Jacque C

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+    <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+            <link href="/css/index.css" rel="stylesheet"> 
+            <link href="/css/nav.css" rel="stylesheet">
+            <link href="/css/indexMobile.css" rel="stylesheet">
+            
+            <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Mrs+Saint+Delafield&family=Nothing+You+Could+Do&family=Parisienne&family=Petit+Formal+Script&family=Reenie+Beanie&display=swap" rel="stylesheet">    <link href="https://fonts.googleapis.com/css2?family=Arapey&family=Brygada+1918:wght@400;700&family=DM+Serif+Text&family=Domine:wght@400;700&family=Faustina:wght@400;700&family=Libre+Baskerville:wght@400;700&family=Roboto+Slab:wght@400;800;900&display=swap" rel="stylesheet">    <link rel="preconnect" href="https://fonts.gstatic.com">
+            <link href="https://fonts.googleapis.com/css2?family=Abel&family=Economica:ital,wght@0,400;0,700;1,400&family=Open+Sans+Condensed:wght@300;700&family=Quicksand:wght@300;400;700&family=Rajdhani:wght@400;700&family=Raleway:wght@200;400;800&family=Saira+Condensed:wght@200;400;700;900&display=swap" rel="stylesheet">    <link href="https://fonts.googleapis.com/css2?family=Arapey&family=Blinker:wght@200;400;700;900&family=Brygada+1918:wght@700&family=DM+Serif+Text&family=Domine:wght@700&family=Faustina:wght@600;700&family=Libre+Baskerville:wght@400;700&family=Roboto+Slab:wght@200;400;500;900&display=swap" rel="stylesheet">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Jacques Chirac</title>
+        </head>
+        <body>
+            
+            
+            <section class="home">
+                <?php include "menu.php" ?>
+                <main>
+                    <div class="menuBtn mobile">Menu</div>
+                    <article>
+                        <div class="overflowH"><p class="paragraphe1">Bonjour, je m'appelle</p></div>
+                        <h1 class="overflowH"><div class="chirac1">Chirac,</div> <div class="jacques">Jacques </div><div class="chirac2">Chirac</div></h1>
+                        <div class="overflowH"><p class="paragraphe2">et je suis un mangeur de <a href="https://www.google.com/search?sxsrf=ALeKk02JNUGW0JBPxm_NrTubKZ_4CVdz3g:1616658729420&source=univ&tbm=isch&q=pomme&sa=X&ved=2ahUKEwiamaiK-8rvAhUQkhQKHTXEB6gQjJkEegQIBxAB&biw=1325&bih=969" target="blank"><span class="pomme">Pomme</span></a></p></div>
+                    </article>
+
+                    <aside > 
+                        <img class="asideImg" src="/img/Chirac_pose.png" alt="Chirac fume"/>
+                    </aside>
+                </main>
+            </section>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
+            <script src="/script/anim.js"></script>
+            <script src="/script/nav.js"></script>
+        </body>
+    </html>
---
+++ menu.php
@@ -0,0 +1,43 @@
+
+
+<header class="header">
+    <div class="container">
+        <div class="logo desktop">
+            <img src="/img/chirac_logo_2.png" alt="logo jacques chirac" />
+        </div>
+
+        <div class="desktop">
+            <div class="photo">
+                <img src="/img/chiracAirline.png" src="chirac dort dans l'avion"/>
+            </div>
+            <p id="job">
+                Président
+            </p>            
+        </div>
+        
+
+        <nav>
+            <ul>
+                <li><a href="/index.php" class="hoverline">Accueil</a></li>
+                <li><a href="./quote.php" class="hoverline">Réplique</a></li>
+                <li><a href="/presidence.php" class="hoverline">Présidence</a></li>
+                <li><a href="/guignols.php" class="hoverline">Guignols</a></li>
+                <li><a href="/culture.php" class="hoverline">Culture Pop</a></li>
+            </ul>
+        </nav>
+
+        <div class="quoteContainer">
+            <p class="quote handfont">
+                
+            </p>
+        </div>
+
+        <div class="socialNet">
+            <ul>
+                <li><a href="https://www.facebook.com/jacques.chirac.921" target="blank"><img src="/img/facebook.png" alt="Logo lien facebook"></a></li>
+                <li><a href="https://www.pinterest.fr/search/pins/?q=chirac&rs=typed&term_meta[]=chirac%7Ctyped" target="blank"><img src="/img/pinterest.png" alt="Logo lien Pinterest"/></a></li>
+                <li><a href="https://www.instagram.com/fuckyeahjacqueschirac/?hl=en" target="blank"><img src="/img/instagram.png" alt="Logo lien Instagram"/></a></li>
+            </ul>
+        </div>
+    </div>
+</header>
